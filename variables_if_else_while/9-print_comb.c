1ae534e1b1e4ba3ce6cec2031ef31a35251d656f
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-21 06:25:52 -0800

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: idoudi mokhtar
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int num;
+
+for (num = 9; num >= 0; num--)
+{
+putchar((num % 10) + '0');
+if (num == 0)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+
+putchar('\n');
+return (0);
+}
