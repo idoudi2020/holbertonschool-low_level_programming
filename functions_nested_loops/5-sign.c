b6b7b7a82ebb7b07a9bcf4019d6fed24125efd03
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-22 14:28:33 -0800

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,9 +8,9 @@
  * print_sign - Prints the sign of a number.
  * @n: The number of which the sign will be printed.
  *
- *Returns 1 and prints + if n is greater than zero
- *Returns 0 and prints 0 if n is zero
- *Returns -1 and prints - if n is less than zero
+ *Return: 1 if the number is greater than zero,
+ *        0 if the number is zero,
+ *       -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
@@ -18,7 +18,7 @@ if (n > 0)
 {
 _putchar('+');
 return (1);
-    }
+}
 else if (n == 0)
 {
 _putchar('0');
