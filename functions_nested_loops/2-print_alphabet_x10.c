dbb39a23f5e0c834bf51555e8b12dc5e73be8997
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-22 12:25:59 -0800

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - Prints 10 times the alphabet, in lowercase,followed by a new line.
+*2 print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
 */
 void print_alphabet_x10(void)
 {
