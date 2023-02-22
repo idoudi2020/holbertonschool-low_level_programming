367d849506b289157cfa8d900c795c4c9b7b00a8
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-22 15:41:14 -0800

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * File: 10-add.c
+ * Auth: idoudi mokhtar
+ */
+ #include "main.h"
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result 
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
