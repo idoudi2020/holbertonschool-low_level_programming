3a3aa08382c574a8ab35639213609df67e176998
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-22 14:04:10 -0800

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: idoudi mokhtar
+ */
+
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
