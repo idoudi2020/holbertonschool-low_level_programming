481306e33162b132ef715b95a32d8034c0499dc6
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-23 13:53:09 -0800

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-mul.c
+ * Auth: idoudi mokhtar
+ */
+
+ #include "main.h"
+
+/**
+ * mul - multiply
+ * @a: first int
+ * @b: second int
+ * Return: product of both ints
+ **/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
