f771c628957666296ff4d753d0f63dc594eb9209
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-24 03:05:15 -0800

    function that prints most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: idoudi mokhtar
+ */
+
+ #include "main.h"
+/**
+ * print_most_numbers - prints numbers 0-9 except 2,4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+int c;
+
+for (c = '0'; c <= '9'; c++)
+{
+if (c != '2' && c != '4')
+_putchar(c);
+}
+_putchar('\n');
+}
