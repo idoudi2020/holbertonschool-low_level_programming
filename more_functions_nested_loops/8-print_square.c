5215e9bebf53bd0ac41af8132d5c961e20a8bfce
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-24 08:10:23 -0800

    fixed bug

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of the squares
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+  int i;
+  int y;
+
+  for (i = 0; i < size; i++)
+    {
+      for (y = 0; y < size; y++)
+	{
+	  _putchar (35);
+	}
+      _putchar ('\n');
+    }
+  if (size <= 0)
+    {
+      _putchar ('\n');
+    }
+}
