372b883a4ecf28fa57375090da53cc10ec62bed3
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-24 07:20:47 -0800

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,15 +3,14 @@
 * Auth: idoudi mokhtar
  */
 
-
-
 #include "main.h"
 
-	/**
-	 * more_numbers - prints numbers 0-14 ten times
-	 *
-	 * Return: void
-	 */
+        /**
+	 ** more_numbers - prints numbers 0-14 ten times
+	 **
+	 ** Return: voi
+	 **
+	 **/
 
 void more_numbers(void)
 {
@@ -21,24 +20,24 @@ i = '0';
 repeat = 0;
 limit = '9';
 
-		for (line = 0; line < 10; line++)
-		{
-		while (repeat < 2)
-		{	
-	  	while (i <= limit)
-		{  
-	        if (limit == '4')
-		_putchar('1');
-	_putchar(i);
-	i++;
-	}
-	repeat++;
-	limit = '4';
-	i = '0';
-	}
-        _putchar('\n');
-	repeat = 0;
-        i = '0';
-        limit = '9';
-	}
+for (line = 0; line < 10; line++)
+{
+while (repeat < 2)
+{
+while (i <= limit)
+{
+if (limit == '4')
+_putchar('1');
+_putchar(i);
+i++;
 }
+repeat++;
+limit = '4';
+i = '0';
+}
+_putchar('\n');
+repeat = 0;
+i = '0';
+limit = '9';
+}
+}					
