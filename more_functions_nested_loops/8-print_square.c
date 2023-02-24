4b96f87abe7e141686d5e64cd683ba6f3d5bd48c
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-24 08:13:52 -0800

    fixed bug

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,19 +9,18 @@
 
 void print_square(int size)
 {
-  int i;
-  int y;
-
-  for (i = 0; i < size; i++)
-    {
-      for (y = 0; y < size; y++)
-	{
-	  _putchar (35);
-	}
-      _putchar ('\n');
-    }
-  if (size <= 0)
-    {
-      _putchar ('\n');
-    }
+int i;
+int y;
+for (i = 0; i < size; i++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar (35);
+}
+_putchar ('\n');
+}
+if (size <= 0)
+{
+_putchar ('\n');
+}
 }
