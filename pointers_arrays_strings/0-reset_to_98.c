bfd077f1bc20a54f53914238aaf9fd90a55c8369
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-27 02:20:26 -0800

    Pointers, arrays and strings

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: idoudi mokhtar
+ */
+
+#include "main.h"
+/**
+ * reset_to_98 - updates de value to 98.
+ * @n: Variable to update.
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
