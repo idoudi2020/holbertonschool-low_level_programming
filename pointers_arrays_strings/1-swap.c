83cfa7339f285005122b8902a4f548ef72460d24
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-27 07:37:57 -0800

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+/*
+ * File: 1-swap.c
+ * Auth: idoudi mokhtar
+ */
+
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: Always 0
+ */
+void swap_int(int *a, int *b);
+
+{
+  int temp;
+  temp = a;
+  a=b;
+  b=temp;
+}
