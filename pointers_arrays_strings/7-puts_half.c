e71246482cb1cd10ba414ad065999405bfce51c9
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-28 04:45:57 -0800

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+/**********************************************
+/*
+* File: 7-puts_half.c
+* Auth: idoudi mokhtar
+*/
+
+#include <main.h>
+#include <studio.h>
+
+/**
+ * puts2 -  half of a string, followed by a new line
+ * @str: string
+ * Return: always 0
+ */
+
+
+void puts_half(char *str)
+{
+int f = 0;
+int n = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+n = f / 2;
+if (f % 2 == 1)
+n++;
+while (str[n] != '\0')
+{
+putchar(str[n]);
+n++;
+}
+putchar('\n');
+}
