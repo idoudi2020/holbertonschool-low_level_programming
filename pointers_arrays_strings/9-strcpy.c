89aac53bb06b585b0146f5a08bb13866db2e3971
idoudi2020 <idoudimokhtar@gmail.com>
2023-02-28 06:25:30 -0800

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -6,13 +6,13 @@
 * _strcpy - function that copy the string
 * @dest: A buffer to copy the string to.
 * @src: The source string to copy.
-* Return: always 0. 
+* Return: always 0.
 */
 
 
 char *_strcpy(char *dest, char *src)
 {
 size_t len = strlen(src);
-strncpy(dest, src , len + 1);
+strncpy(dest, src ,len + 1);
 return (dest);
 }
