8fb8bac0872e78fcdd5f9b889cfa8f954ca1962a
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-01 07:42:01 -0800

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,14 @@
  */
 char *string_toupper(char *s)
 {
-  int x = 0;
-
-  while (s[x] != '\0')
-    {
-      if (s[x] >= 'a' && s[x] <= 'z')
-	{
-	  s[x] = s[x] - 32;
-	}
-      x++;
-    }
-  return (s);
+int x = 0;
+while (s[x] != '\0')
+{
+if (s[x] >= 'a' && s[x] <= 'z')
+{
+s[x] = s[x] - 32;
+}
+x++;
+}
+return (s);
 }
