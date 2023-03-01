7af40946f6c79f7e7a3077e0dbfcd4601b41a980
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-01 07:31:37 -0800

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - function that prints the string in reverse
+ * @s: variable
+ * Return: always 0
+ */
+void print_rev(char *s)
+{
+  int x = 0;
+
+  while (s[x] != '\0')
+    {
+      x++;
+    }
+  x--;
+  while (x >= 0)
+    {
+      _putchar(s[x]);
+      x--;
+    }
+  _putchar('\n');
+}
