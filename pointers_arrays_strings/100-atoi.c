36083da600c38c2f15b9bc2339d1c63ee34759d3
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-01 02:20:05 -0800

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,29 +11,31 @@
 
 int _atoi(char *s)
 {
-	int a = 0;
-	int b = 0;
-        int doto = 1;
+int a = 0;
+int b = 0;
+int doto = 1;
+while ((s[a] < '0' || s[a] > '9') && s[a] != 0)
+{   
+if (s[a] == '-')
+doto *= -1;
+a++;
+}
+while ((s[a] >= '0' && s[a] <= '9') && s[a] != 0)
+{
+if (b >= 0)
+{
+b = b * 10 - (s[a] - '0');
+a++;
 
-        while ((s[a] < '0' || s[a] > '9') && s[a] != 0)
-        {   
-        if (s[a] == '-')
-	doto *= -1;
-        a++;
-        }
-        while ((s[a] >= '0' && s[a] <= '9') && s[a] != 0)
-        {
-        if (b >= 0)
-	{
-	b = b * 10 - (s[a] - '0');
-	a++;
-	}
-        else
-	{
-	b = b * 10 - (s[a] - '0');
-	a++;
-	}
-        }  
-        doto *= -1;
-        return (b * doto);
+
+
+}
+else
+{
+b = b * 10 - (s[a] - '0');
+a++;
+}
+}  
+doto *= -1;
+return (b * doto);
 }
