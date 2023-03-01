026b8eb575240d0557430a1596fa40a2d18c6be2
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-01 06:57:18 -0800

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - Function that copies a string
+ * @dest: variable
+ * @src: variable
+ * @n: number of bytes
+ * Return: always 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int h;
+for (h = 0; h < n && src[h] != '\0'; h++)
+{
+est[h] = src[h];
+}
+for (; h < n; h++)
+dest[h] = '\0';
+return (dest);
+}
