47686b3373f95b40b6ea93219356e1ac95a9b2b6
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-04 06:01:57 -0800

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,21 +8,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  int i, j;
-  int c = 0;
+int i, j;
+int c = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] != 32)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+c++;
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      if (s[i] != 32)
-	{
-	  for (j = 0; accept[j] != '\0'; j++)
-	    {
-	      if (s[i] == accept[j])
-		c++;
-	    }
-	}
-      else
-	return (c);
-    }
-  return (c);
+}
+}
+else
+return (c);
+}
+return (c);
 }
