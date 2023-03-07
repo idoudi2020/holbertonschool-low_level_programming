35e6d8ea471c852d9bec24f64834fa0d28747af9
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-07 06:02:34 -0800

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * _puts_recursion - recursion
- * @s: variable
- */
+* _puts_recursion - recursion
+* @s: variable
+*/
 void _puts_recursion(char *s)
 {
-  if (*s != '\0')
-    {
-      putchar(*s);
-      _puts_recursion(++s);
-    }
-  else
-    {
-      putchar('\n');
-    }
+if (*s != '\0')
+{
+putchar(*s);
+_puts_recursion(++s);
+}
+else
+{
+putchar('\n');
+}
 }
