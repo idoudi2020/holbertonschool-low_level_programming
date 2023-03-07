8ef385c6fbf893621244037b518a44243f321982
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-07 05:55:48 -0800

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - recursion
+ * @s: variable
+ */
+void _puts_recursion(char *s)
+{
+  if (*s != '\0')
+    {
+      putchar(*s);
+      _puts_recursion(++s);
+    }
+  else
+    {
+      putchar('\n');
+    }
+}
