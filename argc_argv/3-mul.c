923cdc30f41148f2786d77cf866af6722f636bb4
idoudi2020 <idoudimokhtar@gmail.com>
2023-03-10 14:10:10 -0800

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program that multiplies two numbers.
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+  int result;
+
+  if (argc == 3)
+    {
+      result = atoi(argv[1]) * atoi(argv[2]);
+      printf("%d\n", result);
+    }
+  else
+    {
+      puts("Error");
+    }
+  return (0);
+}
