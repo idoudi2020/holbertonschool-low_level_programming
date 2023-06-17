9afd9643831e4b472d23d70155bcf958c320b79a
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-17 14:05:53 -0700

    task9

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+  // write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+  //  echo "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
+ echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+  return (1);
+}
