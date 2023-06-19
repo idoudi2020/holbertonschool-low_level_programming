eb9417dbae60d1f8b0414fbd990512df1f20574d
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-19 12:57:10 -0700

    task3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+  char c = 'a';
+  char b = 'A';
+
+
+
+  for (; c <= 'z'; )
+    putchar(c++);
+
+  for (; b<= 'Z';)
+    putchar(b++);
+  putchar('\n');
+
+  return (0);
+}
