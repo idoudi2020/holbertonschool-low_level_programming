b194665e7a482cea3d991ba74a88afbfbd4e9384
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-19 16:22:57 -0700

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+  char N='0';
+
+  for (; N<= '9';)
+    putchar(N++);
+  printf("\n");
+
+  return (0);
+}
