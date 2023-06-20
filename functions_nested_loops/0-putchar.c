f8cc6400696af98c9090171bf1f080aaaf449c98
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-20 06:34:16 -0700

    program that prints _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints _putchar
+ *
+ * Return: Always (Success);
+ */
+int main(void)
+
+
+{
+  char s[]="_putchar";
+  int x=0;
+  for (;x<8;x++)
+    {
+      _putchar (s[x]);
+    }
+
+
+
+  _putchar('\n');
+  return 0;
+}
