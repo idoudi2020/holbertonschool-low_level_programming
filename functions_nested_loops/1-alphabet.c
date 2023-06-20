c93ad1ad843a45b87091a83728b86e2eef2c880c
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-20 07:09:42 -0700

    task1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * main - is the entry point 
- * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ * print_alphabet : prints the alphabet in lowercase, followed by a new line
  * Return: Always 0
  */
 
