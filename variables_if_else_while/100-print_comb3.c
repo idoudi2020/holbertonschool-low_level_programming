52af41da451d19032f73026f25e65327cafe743d
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-20 02:31:27 -0700

    task 100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a serie of numbers with commas
+ *
+ * Return:0  if  (Success);
+ */
+int main(void)
+{
+  int c = '0' , b;
+
+  while (c <= '9')
+    {
+      b = 0;
+      while (b <= '9')
+	{
+	  if(c != b && c<b)
+	    {
+	      putchar(c);
+	      putchar(b);
+	      if (c + b != 17)
+		{
+		  putchar(',');
+		  putchar(' ');
+		}
+
+	    }
+	  b++;
+
+	}
+
+
+
+      c++;
+    }
+
+
+  putchar('\n');
+
+  return (0);
+}
