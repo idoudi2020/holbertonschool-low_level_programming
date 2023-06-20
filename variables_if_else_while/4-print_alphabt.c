4a4126badbc4ab466019bc5bcb6771cc58ba8ca1
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-20 01:45:22 -0700

    task4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -25,3 +25,37 @@ int main(void)
 
   return (0);
 }
+/*********************************************
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*
+* Return: 0
+**/
+/****************************
+int main(void)
+{
+
+
+  for (char c = 'a'; c <= 'z';c++)
+    {
+      if (c == 'e' || c == 'q')
+	{
+	  continue;
+	  //continue it's same c++ that mean skip e and q
+	}
+
+
+      putchar(c);
+
+    }
+
+  putchar('\n');
+
+  return (0);
+}
+*******************************************/
