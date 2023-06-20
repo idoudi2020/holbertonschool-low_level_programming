d2e517264839fa9ec7d238d97710cb42e40e221f
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-20 07:06:02 -0700

    task1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - is the entry point 
+ *
+ * Return: Always 0
+ */
+
+
+void print_alphabet(void)
+{
+  char i = 'a';
+
+
+  while (i <= 'z')
+    {
+      _putchar(i++);
+    }
+  _putchar('\n');
+
+}
