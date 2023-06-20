9421cf32e7ca9a5e2ac418ca762da18fce160849
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-20 01:17:30 -0700

    task7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+  char c = 'z';
+
+
+
+
+  while (c >= 'a')
+    {
+      putchar(c--);
+    }
+
+
+
+
+  putchar('\n');
+
+  return (0);
+}
