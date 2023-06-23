ba4d9e10b328256f8c41225f42713851de3fc17d
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-23 15:20:04 -0700

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+/**
+ * main- Fizz-Buzz
+ *
+ * Return: value
+ */
+int main(void)
+{
+  int a;
+
+  for (a = 1; a <= 100; a++)
+    {
+      if ((a % 3 == 0) && (a % 5 == 0))
+	printf("FizzBuzz");
+      else if (a % 5 == 0)
+	printf("Buzz");
+      else if (a % 3 == 0)
+	printf("Fizz");
+      else
+	printf("%d", a);
+      if (a != 100)
+	printf(" ");
+    }
+  printf("\n");
+  return (0);
+}
