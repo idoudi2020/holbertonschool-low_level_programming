49a1d55f66d784e70e13b6f8f2ee424303db6901
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-23 15:03:36 -0700

    function that prints a square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 
 
 /**
- * more_numbers - a function that prints 10 times the numbers, from 0 to 14
+ * print_square - print square
  *
  * Return: void
  */
@@ -13,10 +13,10 @@
 
 void print_square(int size)
 {
-  int ligne ,colone;
-  for(ligne =1 ; ligne <= size ;ligne++)
+  int ligne , colone;
+  for(ligne =0 ; ligne <= size ;ligne++)
     {
-      for(colone=1 ; colone <= size ;colone++)
+      for(colone=0 ; colone <= size ;colone++)
 	_putchar('#');
       _putchar('\n');
 
