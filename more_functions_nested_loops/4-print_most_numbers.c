5131fc3987770f3e4e44835f1f17a161e04e0b9b
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-23 09:10:23 -0700

    function that prints the numbers, from 0 to 9

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,67 @@
-int main(void)
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - do not print 2 4
+ *
+ * Return: value of print most numbers
+ */
+
+void print_most_numbers(void)
+
 {
   char s[] = "0123456789";
   int x;
-  for (x=0;x<10;x++)
-    if (x!=2 && x!=4)
+
+  for (x=0;x<=9;x++)
+
+    if (x!=2 && x!= 4)
       {
-	putchar (s[x]);
+	putchar(s[x]);
+
       }
-  putchar ('\n');
+
+
+  putchar('\n');
+
+
 }
+
+
+
+/*****on peut le code d'autre maniere
+#include <stdio.h>
+
+int main()
+{
+  char s[] = "01356789";
+  int x;
+  
+     
+      
+  for (x=0;x<=9;x++) 
+
+               putchar(s[x]);
+                putchar('\n');
+         
+         return 0;
+}
+
+autre maniere
+
+#include <stdio.h>
+ void main (void)
+{
+int a = 0;
+
+for (a = 48; a <= 57; a++)
+{
+if (a != 50 && a != 52)
+{
+putchar(a);
+}
+}
+putchar('\n');
+}
+
+************************************/
