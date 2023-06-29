42280636a33eacc41ffd0ded527dde828813a89a
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-29 03:23:08 -0700

    task 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
-
 /**
  * print_square - print square
- *
- * Return: void
+ * @size: int
+ * Return: Square
  */
-
-
 void print_square(int size)
 {
-  int ligne , colone;
-  if (size>0)
-  for(ligne =0 ; ligne <= size ;ligne++)
-    {
-      for(colone=0 ; colone <= size ;colone++)
-	_putchar('#');
-      _putchar('\n');
+  int a, b;
 
-    }
+  if (size > 0)
+    for (a = 0; a < size; a++)
+      {
+	for (b = 0; b < size; b++)
+	  _putchar('#');
+	_putchar('\n');
+      }
   else
     _putchar('\n');
 }
