91c50bf9fba074f19388d525ca3d97286b186946
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-29 07:24:13 -0700

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-  int j, i = 0;
+int j, i = 0;
 
-  while (dest[i] != '\0')
-    i++;
-  for (j = 0; j < n && src[j] != '\0'; j++)
-    dest[i + j] = src[j];
-  return (dest);
+while (dest[i] != '\0')
+i++;
+for (j = 0; j < n && src[j] != '\0'; j++)
+dest[i + j] = src[j];
+return (dest);
 }
