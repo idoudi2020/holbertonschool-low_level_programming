4c97a7ad6915540c9a3badb8ea54943085b444a2
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-29 09:59:06 -0700

    #include "main.h"
    /**
    * leet - 1337 encode
    * @s: string
    * Return: encoded to 1337
    */
    char *leet(char *s)
    {
    int i, j;
    char alp[] = "aeotl";
    char leet[] = "43071";
    
    for (i = 0; s[i] != 0; i++)
    {
    for (j = 0; alp[j] != 0; j++)
    {
    if (s[i] == alp[j] || s[i] == alp[j] - 32)
    s[i] = leet[j];
    }
    }
    return (s);
    }

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * leet - 1337 encode
+ * @s: string
+ * Return: encoded to 1337
+ */
+char *leet(char *s)
+{
+  int i, j;
+  char alp[] = "aeotl";
+  char leet[] = "43071";
+
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      for (j = 0; alp[j] != '\0'; j++)
+	{
+	  if (s[i] == alp[j] || s[i] == alp[j] - 32)
+	    s[i] = leet[j];
+	}
+    }
+  return (s);
+}
