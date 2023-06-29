6f5076d0950742533fda789d0a4fe20ecae59e8f
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-29 07:45:49 -0700

    2-strcpy.c

--- pointers_arrays_strings/2-strcpy.c
+++ pointers_arrays_strings/2-strcpy.c
@@ -8,17 +8,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i =0;
+  int i = 0;
 
-  while ( i < n && *src != '\0')
+  while (i < n && src[i] != '\0')
     {
-      *dest = *src;
+      dest[i] = src[i];
       i++;
     }
+
   while (i < n)
     {
-      *dest= '\0';
+      dest[i] = '\0';
       i++;
     }
-  return (dest);
+
+  return dest;
 }
