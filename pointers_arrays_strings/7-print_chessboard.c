3360cbb12e9a42aa44899c5f7245e00360b2f047
idoudi2020 <idoudimokhtar@gmail.com>
2023-06-30 14:16:04 -0700

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,14 +9,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-  size_t i, j;
+ int  i, j;
 
   for (i = 0; i < 8; i++)
     {
       for (j = 0; j < 8; j++)
 	{
-	  printf("%c", a[i][j]);
+	  _putchar( a[i][j]);
 	}
-      printf("\n");
+      _putchar('\n');
     }
 }
