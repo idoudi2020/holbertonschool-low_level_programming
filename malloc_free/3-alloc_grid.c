57cc4f97b3198fbc8c70f121c49e5073b724e93d
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-11 07:53:07 -0700

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,14 +19,18 @@ int **alloc_grid(int width, int height)
   array = malloc(height * sizeof(int *));
   if (array == NULL)
     {
+
       return (NULL);
     }
 
   for(i = 0; i < height; i++)
     {
       array[i] = malloc(width * sizeof(int));
-     
+      if (array[i] == NULL)
+	free (array[i]);
+
+
     }
+
   return (array);
-  free(**array);
 }
