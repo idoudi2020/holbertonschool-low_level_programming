00c3c22617ff3f04bc59805d2930d8cd6868fc3b
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-11 03:13:57 -0700

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -6,12 +6,17 @@
  * @src: pointer to character
  * Return: pointer to string or NULL if insufficient memory
  */
-
-
-char *_strdup( char *str) {
-  char *src = malloc (strlen (str) + 1);
-  if (str == NULL || src == NULL)
+char *_strdup(char *str)
+{
+  if (str == NULL)
     return NULL;
-  strcpy (src, str);
-  return str;
+
+ 
+  char *src = malloc(strlen(str) + 1);
+  if (src == NULL)
+    return NULL;
+
+  strcpy(src, str);
+
+  return src;
 }
