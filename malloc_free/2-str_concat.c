58f1237320ee34adc02404a15c88ebbffdd66b61
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-11 03:41:41 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,7 +10,14 @@
  */
 char* str_concat( char *s1,  char *s2)
 {
+  if (s1 == NULL)
+    s1 = "";
+  if (s2 == NULL)
+    s2 = "";
+  
   char *result = malloc(strlen(s1) + strlen(s2) + 1);
+  if (result == NULL)
+    return NULL;
   strcpy(result, s1);
   strcat(result, s2);
   return result;
