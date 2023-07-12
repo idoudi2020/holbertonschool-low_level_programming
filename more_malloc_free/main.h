6880739e0371675885077479478103aa0225d67d
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-12 10:14:26 -0700

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -5,6 +5,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-char *function_file(char *dest, char *src);
-char *function_file(char *dest, char *src);
+char *_strcat(char *dest, char *src)
+char *_strcpy(char *v, char *src)
+
 #endif
