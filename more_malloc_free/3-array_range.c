b40625252e6770f955fb7b950209b0a600637ad0
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-12 14:26:19 -0700

    3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,15 +11,15 @@
 int* array_range(int min, int max) {
   int *array;
   if (min > max) 
-    return NULL;
+    return (NULL);
    array = malloc((max - min) * sizeof(int) + 1) ;
   if (! array) 
-    return NULL;
+    return (NULL);
 
     for (int i = 0; i < ((max - min) + 1); i++) 
     array[i] = min + i;
 
-  return array;
+    return (array);
 }
   
 
