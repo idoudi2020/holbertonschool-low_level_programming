10de4530aa4cdf8b1be44e115735bb9ec92a3cbb
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-12 09:31:51 -0700

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -5,5 +5,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 #endif
