c4fb379b87fc3fa3a7a34c9184e192a1dbb2e194
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-12 13:40:12 -0700

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_calloc - function
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: ptr to array.
+ **/
+void*_calloc(unsigned int nmemb, unsigned int size)
+{
+
+  intx;
+
+  if (nmemb == 0 || size == 0)
+    return (0);
+  char* ptr = malloc(nmemb * size);
+  if (ptr == 0)
+    return (0);
+  for (x = 0; x < (nmemb * size); x++) /* we can use memset(ptr, 0, (nmemb * size));*/
+    ptr [x] =  0;
+  return (ptr);
+}
