c56e9428408ec472e487b3083cbf121906f3c0b5
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-12 09:39:45 -0700

    function_file.c

---
+++ more_malloc_free/function_file.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+
+
+
+char *_strcat(char *dest, char *src)
+{
+  char *ptr = dest;
+
+  while (*ptr != '\0')
+    ptr++;
+  while (*src != '\0')
+    {
+      *ptr = *src;
+      ptr++;
+      src++;
+    }
+  *ptr = '\0';
+  return (dest);
+}
+
+char *_strcpy(char *v, char *src)
+{
+  int i;
+
+  for (i = 0; src[i] != '\0'; i++)
+    {
+      dest[i] = src[i];
+    }
+  dest[i] = '\0';
+  return (dest);
+}
