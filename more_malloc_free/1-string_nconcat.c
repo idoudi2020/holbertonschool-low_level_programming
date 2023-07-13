e733f168b159ea6fa4aef8b20a6b4a4d842f5a1f
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-13 09:45:23 -0700

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,11 +15,57 @@ char *string_nconcat(char *s1, char *s2, unsigned int n) {
 
   char *str;
 
+  if (s1 == NULL) s1 = "";
 
-
-
+  if (s2 == NULL) s2 = "";
 
 
   if (n >= strlen(s2))
+    n = strlen(s2);
+
+
+  str = malloc(strlen(s1)+ n + 1);
+  if (str == NULL)
+    {
+      return NULL;
+    }
+  _strcpy(str, s1);
+  _strncat(str, s2, n);
+
   return str;
+}
+/**
+ * _strcpy - copie string
+ * @dest: string
+ * @src: string
+ * Return: string to stdout
+ */
+char *_strcpy(char *dest, char *src)
+{
+  int i;
+
+  for (i = 0; src[i] != '\0'; i++)
+    {
+      dest[i] = src[i];
+    }
+  dest[i] = '\0';
+  return (dest);
+}
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: string
+ * @n: int the max number of bytes from src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+  int j, i = 0;
+
+  while (dest[i] != '\0')
+    i++;
+  for (j = 0; j < n && src[j] != '\0'; j++)
+    dest[i + j] = src[j];
+  return (dest);
 }
