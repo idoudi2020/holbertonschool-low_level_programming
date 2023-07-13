2d39d7887a6c2058bd46527316a862a4c3f51b5e
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-13 08:20:00 -0700

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,23 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n) {
   char *str;
 
 
-  if (s1 == NULL) s1 = "";
 
-  if (s2 == NULL)  s2 = "";
 
 
 
   if (n >= strlen(s2))
-    n = strlen(s2);
-
-
-  str = malloc(strlen(s1)+ n + 1);
-  if (str == NULL)
-    {
-      return NULL;
-    }
-  strcpy(str, s1);
- strncat(str, s2, n);
-
   return str;
 }
