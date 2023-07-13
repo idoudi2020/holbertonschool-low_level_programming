011ef4dfa2f05f89d542d3cd15a1230faf03f4a8
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-13 07:00:07 -0700

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -29,8 +29,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n) {
     {
       return NULL;
     }
-  _strcpy(str, s1);
- _strncat(str, s2, n);
+  strcpy(str, s1);
+ strncat(str, s2, n);
 
   return str;
 }
