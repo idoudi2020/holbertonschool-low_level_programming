8b9487ba25a13ec621859959a15a678caf0657ff
idoudi2020 <idoudimokhtar@gmail.com>
2023-07-21 10:48:58 -0700

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,6 +5,21 @@
 #include <stdio.h>
 
 
+/**
+ * struct print - Struct print
+ *
+ * @type: type to print
+ * @f: The function associated
+ */
+
+typedef struct print
+{
+  char type;
+  void (*f)(va_list args);
+} print_f;
+
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
