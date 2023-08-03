fadb06d1a3f0d6d514f8e47b595d60ee66f12cb0
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-03 08:13:26 -0700

    0-print_dlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,11 +11,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
   size_t count = 0;
 
-  while (h)
+ while (h != NULL)
     {
-      printf("%d\n", h->n);
-      count++;
-      h = h->next;
+printf("%d\n", h->n);
+count++;
+h = h->next;
     }
   return (count);
 }
