f8e1be34cd53b35161ade42a93c6da626a43daa3
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-03 08:29:24 -0700

    betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,16 +11,15 @@
 
 size_t print_list(const list_t *h)
 {
-size_t Nodes = 0;
-do
-{
-if (h->str)
-printf("[%d] %s\n", h->len, h->str);
-else
-printf("[0] (nil)\n");
-Nodes++;
-h = h->next;
-}
-while (h != NULL);
-return (Nodes);
+	size_t Nodes = 0;
+
+	do {
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		Nodes++;
+		h = h->next;
+	} while (h != NULL);
+	return (Nodes);
 }
