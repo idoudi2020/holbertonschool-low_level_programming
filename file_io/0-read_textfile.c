87ddf223f92fea3b4c5cf9b083370b125c86b4d8
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-08 06:11:21 -0700

    file_io

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *read_textfile -reads text from a file and prints it
+ *@filename: name of file to read
+ *@letters : number of bytes to read
+ *
+ *Return: number bytes read/printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  int file
+    ssize_t bytes;
+    char buf[READ_BUF_SIZE *8]
+      if (!filename || !letters)
+	return (0);
+    file = open(filename, O_RDONLY);
+    if (file == -1)
+      {
+	return (0);
+      }
+    bytes = read(file,&buf[0],lettres);
+    bytes = write(STDOUT_FILENO, &buf[0], bytes);
+    close (file);
+
+    return (bytes);
+}
---
+++ file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFFER_SIZE 1024
+
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
