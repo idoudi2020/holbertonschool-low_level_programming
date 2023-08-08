9b7317d32d2e20053962e08045a6fbc5466e5bce
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-08 06:24:13 -0700

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,15 +12,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
   int file;
     ssize_t bytes;
-    char buf[READ_BUF_SIZE * 8];
-      if (!filename || !letters)
+    char buf[BUFFER_SIZE * 8];
+    if (!filename || !letters)
 	return (0);
     file = open(filename, O_RDONLY);
     if (file == -1)
       {
 	return (0);
       }
-    bytes = read(file,&buf[0],lettres);
+    bytes = read(file, &buf[0], lettres);
     bytes = write(STDOUT_FILENO, &buf[0], bytes);
     close (file);
 
