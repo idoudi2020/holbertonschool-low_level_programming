4fd0c47065f23ae33801efe44914f53a14944d9b
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-11 10:04:07 -0700

    hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,8 +1,9 @@
-#ifndef _HASH_TABLES_H
-#define _HASH_TABLES_H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
-#include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
