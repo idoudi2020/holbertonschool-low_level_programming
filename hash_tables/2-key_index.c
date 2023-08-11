bc21c6adbe8df566fe584dc3c999ece8231ad531
idoudi2020 <idoudimokhtar@gmail.com>
2023-08-11 10:07:54 -0700

    2-key_index.c

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,21 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * key_index - gives the index of a key.
+ * @key: the key, a string.
+ * @size: the size of the array of the hash table.
  *
- * Return: hash value
+ * Return: Index number for the key/value pair in the hash table array.
  */
-
-unsigned long int hash_djb2(const unsigned char *str)
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-  unsigned long int hash;
-  int c;
-
-  hash = 5381;
-  while ((c = *str++))
-    {
-      hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-  return (hash);
+  unsigned long int hash_value = hash_djb2(key);
+  unsigned long int index = hash_value % size;
+  return (index);
 }
